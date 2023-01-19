708e0b7a069b54b05aaeb7c7355f61fd2967d36f
boniich <ezequieldbo22@hotmail.com>
2023-01-19 14:34:13 -0300

    feat: add the table, model and controller for food request

---
+++ app/Http/Controllers/Api/FoodController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Food;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class FoodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Food::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'image' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'preparationTime' => 'required',
+        ]);
+
+        $newFood = new Food;
+        $newFood->name = $request->name;
+        $newFood->description = $request->description;
+        $newFood->image = $request->image;
+        $newFood->price = $request->price;
+        $newFood->preparation_time = $request->preparationTime;
+        $newFood->save();
+
+        return $newFood;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\FoodController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,8 @@ use App\Http\Controllers\Api\UserController;
 
 Route::post('register',[UserController::class,'register']);
 Route::post('login',[UserController::class, 'login']);
+
+Route::apiResource('foods',FoodController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
